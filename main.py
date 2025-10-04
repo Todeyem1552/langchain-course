c1d85e81001f235e7aa42af9c05fd8581cc7fc12
Tobiloba <todeyemi27@gmail.com>
2025-10-04 03:15:37 +0100

    push main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from itertools import chain
 import os
 from dotenv import load_dotenv
+load_dotenv()
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
@@ -9,7 +10,7 @@ from langchain_tavily import TavilySearch
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(temperature=0, model="gpt-4")
-react_prompt = hub.pull("hwchase17/react-agent").get_prompt(tools)
+react_prompt = hub.pull("hwchase17/react")
 agent = create_react_agent(
     llm=llm, 
     tools=tools, 
@@ -17,11 +18,9 @@ agent = create_react_agent(
 )
 agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
 
-load_dotenv()
-
 def main():
     print("Hello from langchain-course!")
-    result = chain.invoke(
+    result = agent_executor.invoke(
         input={
             "input": "Search for 3 job posting for AI engineer using Langchain in Nigeria from LinkedIn and list their details",
             }
