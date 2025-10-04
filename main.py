2783fe21e366a78832c3d6f5f3bd27f0c26508f1
Tobiloba <todeyemi27@gmail.com>
2025-10-04 03:51:57 +0100

    ReAct Prompt: added output parsing

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
-from itertools import chain
 import os
+from itertools import chain
+
 from dotenv import load_dotenv
+
 load_dotenv()
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
 tools = [TavilySearch()]
 llm = ChatOpenAI(temperature=0, model="gpt-4")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
 agent = create_react_agent(
-    llm=llm, 
-    tools=tools, 
-    prompt=react_prompt
+    llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
 )
-agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
+agent_executor = AgentExecutor.from_agent_and_tools(
+    agent=agent, tools=tools, verbose=True
+)
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+
+chain = agent_executor | extract_output | parse_output
 
 def main():
     print("Hello from langchain-course!")
-    result = agent_executor.invoke(
+    result = chain.invoke(
         input={
             "input": "Search for 3 job posting for AI engineer using Langchain in Nigeria from LinkedIn and list their details",
-            }
+        }
     )
     print(result)
 
